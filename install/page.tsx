6f4837998e8453b173b91eb5854797b1a24f3144
grazinggrass <cal@grazinggrass.com>
2025-06-06 14:03:36 -0500

    Create page.tsx

---
+++ install/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function InstallPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const accessToken = params.get('access_token');
+    const locationId = params.get('location_id');
+
+    if (accessToken && locationId) {
+      router.push(`/blog-selector?location_id=${locationId}&access_token=${accessToken}`);
+    }
+  }, []);
+
+  return <p className="text-center mt-20 text-gray-600">Setting up your app...</p>;
+}

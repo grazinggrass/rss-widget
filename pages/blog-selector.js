18ed7cebc55276f5a74ef954619bca71e515b0c6
grazinggrass <cal@grazinggrass.com>
2025-06-06 14:20:23 -0500

    Delete pages directory & Remove duplicate JS route file

--- pages/blog-selector.js
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const GhlBlogSelector = dynamic(() => import('../components/GhlBlogSelector'), { ssr: false });
-
-export default function BlogSelectorPage() {
-  return <GhlBlogSelector />;
-}

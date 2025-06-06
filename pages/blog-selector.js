af242cb26b3dd3ca57fd8c03fbf663c31be6c56e
grazinggrass <cal@grazinggrass.com>
2025-06-06 13:40:09 -0500

    Create blog-selector.js

---
+++ pages/blog-selector.js
@@ -0,0 +1,6 @@
+// pages/blog-selector.js
+import GhlBlogSelector from '@/components/GhlBlogSelector'; // Adjust the path if needed
+
+export default function BlogSelectorPage() {
+  return <GhlBlogSelector />;
+}
